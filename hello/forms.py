2cbf025f56d1d3e134736331af73527386c3ce74
Suehiro20 <96360161+Suehiro20@users.noreply.github.com>
2022-08-02 18:46:15 +0900

    こまめにしておらず申し訳ありません

--- hello/forms.py
+++ hello/forms.py
@@ -1,5 +1,40 @@
 from django import forms
 
+class FirstForm(forms.Form):
+    name = forms.CharField(label='name')
+    mail = forms.EmailField(label='mail')
+    age = forms.IntegerField(label='age')
+
+class SecondForm(forms.Form):
+    check = forms.BooleanField(label='Checkbox', required=False)
+
+class ThirdForm(forms.Form):
+    check = forms.NullBooleanField(label='Check')
+
+class FourthForm(forms.Form):
+    data = [
+        ('値', 'ラベル'),
+        ('one', 'item 1'),
+        ('two', 'item 2'),
+    ]
+    choice = forms.ChoiceField(label='Choice', choices=data)
+
+class FifthForm(forms.Form):
+    data = [
+        ('値', 'ラベル'),
+        ('one', 'item 1'),
+        ('two', 'item 2'),
+    ]
+    choice = forms.ChoiceField(label='Radio', choices=data, widget=forms.RadioSelect())
+
+class SixthForm(forms.Form):
+    data = [
+        ('値', 'ラベル'),
+        ('one', 'item 1'),
+        ('two', 'item 2'),
+    ]
+    choice = forms.ChoiceField(label='List', choices=data, widget=forms.Select(attrs={'size': 3}))
+
 class HelloForm(forms.Form):
     data = [
         ('値', 'ラベル'),
@@ -9,4 +44,7 @@ class HelloForm(forms.Form):
         ('four', 'item 4'),
         ('five', 'item 5'),
     ]
-    choice = forms.MultipleChoiceField(label='radio', choices=data, widget=forms.SelectMultiple(attrs={'size': 5}))
+    choice = forms.MultipleChoiceField(label='Multiple', choices=data, widget=forms.SelectMultiple(attrs={'size': 5}))
+
+class IdForm(forms.Form):
+    id = forms.IntegerField(label='ID')
