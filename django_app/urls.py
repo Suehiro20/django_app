b135eee19735f1a9d460af3fb9b1c72a38ca6f6c
Suehiro20 <96360161+Suehiro20@users.noreply.github.com>
2023-03-26 04:44:19 +0900

    blogapp/use bootstrap

--- django_app/urls.py
+++ django_app/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('hello/', hello.index),
     path('hello/', include('hello.urls')),
+    path('blogapp/', include('blogapp.urls')),
 ]
