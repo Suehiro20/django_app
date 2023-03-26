59b55989cfa9800d3756761dcffc038d1d490cb3
Suehiro20 <96360161+Suehiro20@users.noreply.github.com>
2023-03-26 17:51:23 +0900

    show by category

--- blogapp/views.py
+++ blogapp/views.py
@@ -8,7 +8,29 @@ class Index(ListView):
     template_name = 'index.html'
     context_object_name = 'orderby_records'
     queryset = BlogPost.objects.order_by('-posted_at')
+    paginate_by = 4
 
 class BlogDetail(DetailView):
     template_name = 'post.html'
     model = BlogPost
+
+class ScienceView(ListView):
+    template_name = 'science_list.html'
+    model = BlogPost
+    context_object_name = 'science_records'
+    queryset = BlogPost.objects.filter(category='science').order_by('-posted_at')
+    paginate_by = 2
+
+class DailylifeView(ListView):
+    template_name = 'dailylife_list.html'
+    model = BlogPost
+    context_object_name = 'dailylife_records'
+    queryset = BlogPost.objects.filter(category='dailylife').order_by('-posted_at')
+    paginate_by = 2
+
+class MusicView(ListView):
+    template_name = 'music_list.html'
+    model = BlogPost
+    context_object_name = 'music_records'
+    queryset = BlogPost.objects.filter(category='music').order_by('-posted_at')
+    paginate_by = 2
